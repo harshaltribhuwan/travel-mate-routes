78aef48dc1daee617aabc116cc99ab98ac3355d8
Harshal Tribhuwan <harshaltribhuwan12345@gmail.com>
2025-05-27 18:41:02 +0530

    changed img name

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'imag-192.png',
+            src: 'img-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'imag-512.png',
+            src: 'img-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
