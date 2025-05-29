75041d9ea459748412a75e7da5b2a764351ac98e
Harshal Tribhuwan <harshaltribhuwan12345@gmail.com>
2025-05-30 03:33:25 +0530

    build  optimisation

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
     react(),
+    visualizer(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
